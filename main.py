ef696d2e27cbef75d0e3e310930adc7cfbd783ea
hootmole <jachym1137@gmail.com>
2023-10-17 23:58:06 +0200

    it kinda works now

--- main.py
+++ main.py
@@ -63,23 +63,23 @@ class Brick:
         self.static_mesh = static_mesh
         self.distance_to_ball = None
         self.ball_collision_point = None
-        self.slope_angle = None
+        self.slope_angles = []
 
     def draw(self):
         if self.isAlive:
             vertex_table = self.static_mesh.vertex_table
-            for edge_index in range(len(self.static_mesh.vertex_table) - 1):
-                pygame.draw.line(self.win, self.color, reverseY(sum(vertex_table[edge_index], self.pos)), reverseY(sum(vertex_table[edge_index + 1], self.pos)))
-            # finish by connecting the first and last vertex
-            pygame.draw.line(self.win, self.color, reverseY(sum(vertex_table[0], self.pos)), reverseY(sum(vertex_table[-1], self.pos)))
+            for edge_index in range(len(vertex_table)):
+                pygame.draw.line(self.win, self.color, reverseY(sum(vertex_table[edge_index - 1], self.pos)), reverseY(sum(vertex_table[edge_index], self.pos)))
 
-    def calculate_ball_collision_point(self, ball: Ball):
+
+    def handle_collision(self, ball: Ball):
         vertex_table = self.static_mesh.vertex_table
 
         ball_collisions_points_list = []
         distances_to_ball = []
+        self.slope_angles = []
 
-        for edge_index in range(len(self.static_mesh.vertex_table)):
+        for edge_index in range(len(vertex_table)):
             # assign 2 working vertexes, assuming that 2 vertexes next to each other in vertex table are connected with edge that is linear function
             vertex1 = sum(vertex_table[edge_index - 1], self.pos) # P1
             vertex2 = sum(vertex_table[edge_index], self.pos) # P2
@@ -88,8 +88,16 @@ class Brick:
             d_vertex_y = vertex2[1] - vertex1[1]
 
             vertex_distance = math.sqrt(d_vertex_x ** 2 + d_vertex_y ** 2)
-            slope_angle = math.atan2(d_vertex_y, d_vertex_x) # alpha
-            self.slope_angle = slope_angle
+
+            # if d_vertex_x is zero (vertexes are above each other) set the slope angle to pi/2 (90deg)
+            # if d_vertex_x == 0:
+            #     slope_angle = math.pi / 2
+            # else:
+            #     slope_angle = math.atan2(d_vertex_y, d_vertex_x)
+
+            slope_angle = math.atan2(d_vertex_y, d_vertex_x)
+
+            self.slope_angles.append(slope_angle)
 
             distance = math.cos(slope_angle) * (vertex1[1] - ball.pos[1]) - math.sin(slope_angle) * (vertex1[0] - ball.pos[0])
 
@@ -101,25 +109,19 @@ class Brick:
             distances_to_ball.append(abs(distance))
         
         # find the smallest edge distance and use the values
-        smallest_distance = min(distances_to_ball)
-        smallest_distance_index = distances_to_ball.index(smallest_distance)
-
-        self.distance_to_ball = smallest_distance
-        self.ball_collision_point = ball_collisions_points_list[smallest_distance_index] 
-
-
+        distance_to_ball = min(distances_to_ball)
+        working_edge = distances_to_ball.index(distance_to_ball) # edge that is colliding with ball
         
+        working_slope_angle = self.slope_angles[working_edge]
+        self.ball_collision_point = ball_collisions_points_list[working_edge] 
 
-    def handle_collision(self, ball: Ball):
-        self.calculate_ball_collision_point(ball)
+
+        # collision handeling part
         velocity = ball.vel
-
-        # check if colliding
-        if self.distance_to_ball <= ball.size:
-            # rotate velocity vector by slope angle of function at collision point
-            # mirror y axis by multiplying y value by -1
-
-            normal_vector_angle = self.slope_angle + math.pi / 2
+        
+        if distance_to_ball <= ball.size: 
+            # check if colliding
+            normal_vector_angle = working_slope_angle + math.pi / 2
             normal_vector = [math.cos(normal_vector_angle), math.sin(normal_vector_angle)]
 
             dot = velocity[0] * normal_vector[0] + velocity[1] * normal_vector[1]
@@ -139,7 +141,7 @@ class Brick:
             vertex1 = sum(vertex_table[edge_index - 1], self.pos) # P1
             vertex2 = sum(vertex_table[edge_index], self.pos) # P2
 
-            normal_angle = self.slope_angle + (math.pi / 2)
+            normal_angle = self.slope_angles[edge_index] + (math.pi / 2)
 
             normal_vector = [math.cos(normal_angle) * lenght, math.sin(normal_angle) * lenght]
             origin = [
@@ -150,8 +152,7 @@ class Brick:
             normal_vector_left = sum(normal_vector, origin)
             normal_vector_right = sum([-normal_vector[0], -normal_vector[1]], origin)
 
-            pygame.draw.line(win, "white", reverseY(origin), reverseY(normal_vector_left))
-            pygame.draw.line(win, "white", reverseY(origin), reverseY(normal_vector_right))
+            pygame.draw.line(win, "orange", reverseY(normal_vector_left), reverseY(normal_vector_right), 1)
 
 
 
@@ -159,8 +160,8 @@ class Brick:
 
 
 line = Mesh([
-    [0, 200],
-    [0, -200],
+    [-100, 200],
+    [177, -200],
     ])
 
 square = Mesh([
@@ -174,10 +175,9 @@ walls_mesh = Mesh([
     [1, height - 1],
     [width - 1, height - 1],
     [width - 1, 1],
-    [1, 1]
+    [1, 1],
 ])
 
-lol = Brick([100, 300], square, True, "orange", win)
 
 ball = Ball([30, 300], [1, 2.0], 10, [0, 0, 255], win)
 brick = Brick([width / 2, height / 2], line, True, "yellow", win)
@@ -249,9 +249,6 @@ while run:
     walls.handle_collision(ball)
     walls.draw_normal_vector(30, win)
 
-    lol.draw()
-    lol.handle_collision(ball)
-    lol.draw_normal_vector(30, win)
 
     # draw fps counter
     end_time = pygame.time.get_ticks()  # Get the end time of the frame
@@ -273,7 +270,7 @@ while run:
 
 
     y += 1.5
-    # brick.static_mesh.vertex_table[1] = [brick.static_mesh.vertex_table[1][0], y]
+    #brick.static_mesh.vertex_table[1] = [y, brick.static_mesh.vertex_table[1][0]]
 
     if y > height:
         y = 0
